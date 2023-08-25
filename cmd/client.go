b6be4cae99085f37f5e6b3a5d42a057802fabe81
yhw <2278069802@qq.com>
2023-08-25 15:44:46 +0800

    update client command

--- cmd/client.go
+++ cmd/client.go
@@ -24,13 +24,9 @@ import (
 // clientCmd represents the client command
 var clientCmd = &cobra.Command{
 	Use:   "client",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A websocket client",
+	Long: `
+A websocket client`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("client called")
 	},
@@ -38,14 +34,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(clientCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// clientCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// clientCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
